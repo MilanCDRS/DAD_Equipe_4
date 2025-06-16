44c5c86a42118f14aa3d2c37f2651db4972182eb
Neskai <ines.kalam.alami@gmail.com>
2025-06-16 10:18:06 +0200

    Fx-36-Ne plus stocker l'access token dans localstorage mais dans un cookie grace à js-cookie
    https://github.com/MilanCDRS/DAD_Equipe_4/issues/42

--- Breezy/breezy-frontend/src/app/layout.js
+++ Breezy/breezy-frontend/src/app/layout.js
@@ -6,28 +6,11 @@ import "./globals.css";
 import { useEffect } from "react";
 import { rehydrate } from "@/store/authSlice";
 
-function Rehydrate() {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      const token = window.localStorage.getItem("token");
-      const user = JSON.parse(window.localStorage.getItem("user") || "null");
-      if (token && user) {
-        dispatch(rehydrate({ user, token }));
-      }
-    }
-  }, [dispatch]);
-  return null;
-}
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider store={store}>
-          <Rehydrate />
-          {children}
-        </Provider>
+        <Provider store={store}>{children}</Provider>
       </body>
     </html>
   );
