b0c52c730d77a51f9c18a42669ed7800ea45c73e
khady <mbaye.sokhnakhady@viacesi.fr>
2025-06-18 11:15:47 +0200

    front home page

--- Breezy/breezy-frontend/src/app/Components/navbar.js
+++ Breezy/breezy-frontend/src/app/Components/navbar.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar() {
     return(
         <div className="bg-white shadow-md p-4 flex flex-col items-center">
-            <nav className="container mx-auto cover flex justify-center items-center space-x-30">
+            <nav className="container mx-auto cover flex justify-center items-center space-x-24">
                 <Link href="/profile" className="text-black hover:text-blue-500 ">
                     <UserIcon className="inline-block mr-2" size={24}  color="black"/>
 
--- Breezy/breezy-frontend/src/app/layout.js
+++ Breezy/breezy-frontend/src/app/layout.js
@@ -5,12 +5,14 @@ import store from "@/store/store";
 import "./globals.css";
 import { useEffect } from "react";
 import { rehydrate } from "@/store/authSlice";
+import PageTest from "./Components/pagetest";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
       <body>
-        
+        <PageTest />
         <Provider store={store}>{children}
          
         </Provider>
